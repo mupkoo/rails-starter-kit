b7d06c23dfb59e7c134decd2795c94e0d93fb014
Mirko Akov <mupkoo@gmail.com>
2014-12-22 10:56:45 +0200

    Favor JavaScript instead of Coffee

---
+++ app/assets/javascripts/active_admin.js
@@ -0,0 +1,12 @@
+//= require active_admin/base
+//= require redactor-rails
+//= require libs/chosen
+//= require_self
+
+$(function () {
+
+    $('select').chosen({
+        allow_single_deselect: true
+    });
+
+});
--- app/assets/javascripts/active_admin.js.coffee
+++
@@ -1,7 +0,0 @@
-#= require active_admin/base
-#= require redactor-rails
-#= require libs/chosen
-#= require_self
-
-$ ->
-    $('select').chosen({ allow_single_deselect: true })
