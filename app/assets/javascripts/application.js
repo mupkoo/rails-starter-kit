613125a283bcab1c0769ed17c306ab22d916815d
Mirko Akov <mupkoo@gmail.com>
2014-12-22 10:07:26 +0200

    Favor JavaScript instead of Coffee

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require bootstrap/transition
+//= require bootstrap/collapse
+//= require bootstrap/alert
+
+// Insert your scripts here
+
+//= require turbolinks
