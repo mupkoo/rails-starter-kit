074f578e284d6921841ddb88b78c62e49f2cc176
Mirko Akov <mupkoo@gmail.com>
2014-12-24 10:53:39 +0200

    Add contacts page, mailer layout and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
     devise_for :users
 
+    get 'contacts', to: 'contacts#index', as: :contacts
+    post 'contacts', to: 'contacts#create'
+
     root to: 'home#index'
 
 end
